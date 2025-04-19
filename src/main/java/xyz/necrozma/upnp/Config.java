cf0c60aad428c16ccf2c42a97f304869c6c56ea3
DobruiKola <testtestovyj514@gmail.com>
2025-04-19 19:29:47 +0300

    Update Config.java

--- src/main/java/xyz/necrozma/upnp/Config.java
+++ src/main/java/xyz/necrozma/upnp/Config.java
@@ -7,71 +7,73 @@ import dev.dejvokep.boostedyaml.settings.dumper.DumperSettings;
 import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
 import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
 import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
-import org.bukkit.plugin.java.JavaPlugin;
+import net.fabricmc.loader.api.FabricLoader;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Objects;
-
-import static org.bukkit.Bukkit.getLogger;
 
 public class Config {
 
     private static Config instance;
     private YamlDocument config;
 
-    private Config(JavaPlugin plugin) {
+    private Config() {
         try {
-            // Load the config file, or create it if it doesn't exist
-             config = YamlDocument.create(new File(plugin.getDataFolder(), "config.yml"),
-                     Objects.requireNonNull(plugin.getResource("config.yml")), GeneralSettings.DEFAULT, LoaderSettings.builder().setAutoUpdate(true).build(), DumperSettings.DEFAULT, UpdaterSettings.builder().setVersioning(new BasicVersioning("config-version")).build());
+            File configDir = new File(FabricLoader.getInstance().getConfigDir().toFile(), "upnp");
+            if (!configDir.exists()) configDir.mkdirs();
+
+            File configFile = new File(configDir, "config.yml");
+
+            // Пытаемся загрузить embedded config.yml из ресурсов
+            InputStream defaultConfig = Objects.requireNonNull(
+                    getClass().getClassLoader().getResourceAsStream("config.yml"),
+                    "Не удалось найти config.yml в resources!"
+            );
+
+            config = YamlDocument.create(
+                    configFile,
+                    defaultConfig,
+                    GeneralSettings.DEFAULT,
+                    LoaderSettings.builder().setAutoUpdate(true).build(),
+                    DumperSettings.DEFAULT,
+                    UpdaterSettings.builder()
+                            .setVersioning(new BasicVersioning("config-version"))
+                            .build()
+            );
 
         } catch (IOException e) {
-            plugin.getLogger().severe("Could not load config.yml: " + e.getMessage());
+            System.err.println("[UPNP] Ошибка загрузки config.yml: " + e.getMessage());
         }
-    }
-
-    // Singleton instance retrieval
-    public static Config getInstance(JavaPlugin plugin) {
-        if (instance == null) {
-            instance = new Config(plugin);
-        }
-        return instance;
     }
 
     public static Config getInstance() {
-        if (instance == null) {
-            throw new IllegalStateException("Config has not been initialized. Call getInstance(JavaPlugin) first.");
-        }
+        if (instance == null) instance = new Config();
         return instance;
     }
 
-    // Method to get a boolean value from the config
     public boolean getBoolean(Route route) {
         return config.getBoolean(route);
     }
 
-    // Method to get a string value from the config
     public String getString(Route route) {
         return config.getString(route);
     }
 
-    // Method to get a specific integer value from the config
     public int getInt(Route route) {
         return config.getInt(route);
     }
 
-    // set a value in the config
     public void set(Route route, Object value) {
         config.set(route, value);
     }
 
-    // save the config
     public void save() {
         try {
             config.save();
         } catch (IOException e) {
-            getLogger().severe("Could not save config.yml: " + e.getMessage());
+            System.err.println("[UPNP] Ошибка сохранения config.yml: " + e.getMessage());
         }
     }
 }
